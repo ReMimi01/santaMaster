54eb1e2ca284adaa68e979c465cde4afae36e5bd
remiman <remi.mandron@gmail.com>
2019-12-19 16:34:54 +0100

    resolution conflit

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -3,21 +3,15 @@ import { Routes, RouterModule } from '@angular/router';
 import { CalendarComponent } from './pages/calendar/calendar.component'
 import { LandingPagesComponent } from './pages/landing-pages/landing-pages.component';
 import { FormulaireComponent } from './pages/formulaire/formulaire.component';
-<<<<<<< HEAD
 import { AddVotesComponent } from './pages/add-votes/add-votes.component';
-=======
 import { RankingPageComponent } from './pages/ranking-page/ranking-page.component';
->>>>>>> main
 
 const routes: Routes = [
   { path :'', component : LandingPagesComponent},
   { path :'formulaire', component : FormulaireComponent},
   { path: 'calendar', component: CalendarComponent },
-<<<<<<< HEAD
-  { path: 'ba', component: AddVotesComponent }
-=======
+  { path: 'ba', component: AddVotesComponent },
   { path: 'classement', component: RankingPageComponent }
->>>>>>> main
 ];
 
 @NgModule({
